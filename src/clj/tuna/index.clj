010d0afd1d87edd32f423288b025467633f72088
Samrat Man Singh <samratmansingh@gmail.com>
2013-02-14 16:57:29 +0545

    Move search input

--- src/clj/tuna/index.clj
+++ src/clj/tuna/index.clj
@@ -8,15 +8,11 @@
                 (include-css "/bootstrap/css/bootstrap.css")
                 (include-css "/css/font-awesome.min.css")
                 (include-js "/js/main.js")
-                "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+                "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0\">"
                 "<meta name=\"apple-mobile-web-app-capable\" content=\"yes\">"]
                [:body
                 [:div {:class "navbar navbar-fixed-bottom"}
                  [:div {:class "navbar-inner"}
-                   [:input {:type "text"
-                            :id "query"
-                            :class "navbar-search pull-right search-query"
-                            :placeholder "Search"}]
                   [:div
                    [:i {:id "prev-song" :class "icon-step-backward" :style "font-size:2em;padding:5px;"}]
                    [:i {:id "play-pause" :class "icon-play" :style "font-size:2em;padding:5px;"}]
@@ -24,8 +20,12 @@
                   [:div {:class "title"} "&nbsp;"]
                   [:div [:div {:class "progress progress-striped"}
                          [:div {:class "bar"}]
-                         [:div {:id "current" :style "text-align:right;"} "00:00 / 00:00"]]
-                   ]]] 
+                         [:div {:id "current" :style "text-align:right;"}
+                          "00:00 / 00:00"]]]]]
+                [:div {:class "container"} [:input {:type "text"
+                                                    :id "query"
+                                                    :class "navbar-search search-query"
+                                                    :placeholder "Search"}]]
                 [:audio {:id "player"
                          :preload "auto"
                          :ontimeupdate "tuna.main.show_current_time(this.currentTime,this.duration);"}]]
