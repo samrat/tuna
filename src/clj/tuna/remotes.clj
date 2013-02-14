cbf3a2fe633cc60e9f486c1bd7730400267230cd
Samrat Man Singh <samratmansingh@gmail.com>
2013-02-14 23:08:51 +0545

    Remove remotes.clj file.

--- src/clj/tuna/remotes.clj
+++
@@ -1,9 +0,0 @@
-(ns tuna.remotes
-  (:require [cemerick.shoreleave.rpc :refer (defremote)])
-  (:use tuna.db))
-
-(defremote song-info [id]
-  (song-by-id id))
-
-(defremote songs []
-  (all-songs))
